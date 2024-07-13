50ad175d62a4c91ee7bb2fe1acfac7d797669598
oleg <durkin_oleg@mail.ru>
2024-07-13 21:31:42 +0300

    Создание проекта

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-def f():
-    pass
+import sqlite3
+from PyQt5 import QtWidgets, uic
+
+
+class CoffeeApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(CoffeeApp, self).__init__()
+        uic.loadUi('main.ui', self)
+        self.load_data()
+
+    def load_data(self):
+        connection = sqlite3.connect('coffee.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM coffee")
+        rows = cursor.fetchall()
+        self.tableWidget.setRowCount(len(rows))
+        self.tableWidget.setColumnCount(len(rows[0]))
+        for i, row in enumerate(rows):
+            for j, val in enumerate(row):
+                self.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(val)))
+        connection.close()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = CoffeeApp()
+    window.show()
+    sys.exit(app.exec_())
